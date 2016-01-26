f44622a22fbacf435dc30ceed95caad93e28294c
4jchc <jiangjin5y24@163.com>
2016-01-26 11:46:46 +0800

    【WeiBo-Pods】01初始化项目

---
+++ 4jchc-WeiBo-Pods/4jchc-WeiBo-Pods/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  4jchc-WeiBo-Pods
+//
+//  Created by 蒋进 on 16/1/26.
+//  Copyright © 2016年 蒋进. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
