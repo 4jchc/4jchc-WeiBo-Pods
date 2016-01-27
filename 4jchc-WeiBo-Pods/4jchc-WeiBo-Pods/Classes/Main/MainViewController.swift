7747dd2a583282159a740949a61b91c972fd4e1e
4jchc <jiangjin5y24@163.com>
2016-01-27 13:26:15 +0800

    【WeiBo-Pods】03  1.命名空间修改:product name 2.动态获取命名空间infoDictionary!["CFBundleExecutable"] 3.添加本地json数据

--- 4jchc-WeiBo-Pods/4jchc-WeiBo-Pods/Classes/Main/MainViewController.swift
+++ 4jchc-WeiBo-Pods/4jchc-WeiBo-Pods/Classes/Main/MainViewController.swift
@@ -14,8 +14,12 @@ command + shift + j -> 定位到当前文件的目录结构
 command + n 创建文件
 */
 class MainViewController: UITabBarController {
-
-   
+    /*
+    1.命名空间修改:product name
+    2.动态获取命名空间infoDictionary!["CFBundleExecutable"]
+    3.通过服务器json来动态加载节日标题图片
+    */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // 设置当前控制器对应tabBar的颜色
@@ -42,12 +46,16 @@ class MainViewController: UITabBarController {
         addChildViewController(nav)
         */
         
-        addChildViewController(HomeViewController(), title: "首页", imageName: "tabbar_home")
-        addChildViewController(MessageViewController(), title: "消息", imageName: "tabbar_message_center")
-        addChildViewController(DiscoverViewController(), title: "广场", imageName: "tabbar_discover")
-        addChildViewController(ProfileViewController(), title: "我", imageName: "tabbar_profile")
+        
+        // 2.添加子控制器
+        addChildViewController("HomeTableViewController", title: "首页", imageName: "tabbar_home")
+        addChildViewController("MessageTableViewController", title: "消息", imageName: "tabbar_message_center")
+        addChildViewController("DiscoverTableViewController", title: "广场", imageName: "tabbar_discover")
+        addChildViewController("ProfileTableViewController", title: "我", imageName: "tabbar_profile")
         
     }
+    
+    
     /**
      初始化子控制器
      
@@ -55,21 +63,40 @@ class MainViewController: UITabBarController {
      :param: title           子控制器的标题
      :param: imageName       子控制器的图片
      */
-    private func addChildViewController(childController: UIViewController, title:String, imageName:String) {
+     //    private func addChildViewController(childController: UIViewController, title:String, imageName:String) {
+    
+    private func addChildViewController(childControllerName: String, title:String, imageName:String) {
+        // <DSWeibo.HomeTableViewController: 0x7ff3a15967e0>
+        //        print(childController)
+        
+        //MARK: - -1.动态获取命名空间
+        let ns = NSBundle.mainBundle().infoDictionary!["CFBundleExecutable"] as! String
+        
+        // 0 .将字符串转换为类
+        // 0.1默认情况下命名空间就是项目的名称, 但是命名空间名称是可以修改的
+        let cls:AnyClass? = NSClassFromString(ns + "." + childControllerName)
+        // 0.2通过类创建对象
+        // 0.2.1将AnyClass转换为指定的类型
+        let vcCls = cls as! UIViewController.Type
+        // 0.2.2通过class创建对象
+        let vc = vcCls.init()
+        
+        // 0.1通过类创建一个对象
+        
         
         // 1设置首页对应的数据
-        childController.tabBarItem.image = UIImage(named: imageName)
-        childController.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
-        childController.title = title
+        vc.tabBarItem.image = UIImage(named: imageName)
+        vc.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
+        vc.title = title
         
         // 2.给首页包装一个导航控制器
         let nav = UINavigationController()
-        nav.addChildViewController(childController)
+        nav.addChildViewController(vc)
         
         // 3.将导航控制器添加到当前控制器上
         addChildViewController(nav)
+        
     }
     
-
-
+    
 }
---
+++ 4jchc-WeiBo-Pods/4jchc-WeiBo-Pods/Classes/Main/Resource/NullViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MessageViewController.swift
+//  4jchc-WeiBo-Pods
+//
+//  Created by 蒋进 on 16/1/27.
+//  Copyright © 2016年 蒋进. All rights reserved.
+//
+
+import UIKit
+
+class NullViewController: UIViewController {
+
+    
+}
