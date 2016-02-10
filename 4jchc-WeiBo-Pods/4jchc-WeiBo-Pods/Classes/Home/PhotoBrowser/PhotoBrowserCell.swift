47cf06c5592365e4512a12ba88fa5531f9442fdc
4jchc <jiangjin5y24@163.com>
2016-02-10 14:04:12 +0800

    【WeiBo-Pods】55    图片缩放 添加菊花     1.设置scrollview的代理     2.cell重用要还原设置     3.下载图片之前-添加菊花标志

--- 4jchc-WeiBo-Pods/4jchc-WeiBo-Pods/Classes/Home/PhotoBrowser/PhotoBrowserCell.swift
+++ 4jchc-WeiBo-Pods/4jchc-WeiBo-Pods/Classes/Home/PhotoBrowser/PhotoBrowserCell.swift
@@ -14,28 +14,41 @@ class PhotoBrowserCell: UICollectionViewCell {
     var imageURL: NSURL?
         {
         didSet{
-            //            iconView.sd_setImageWithURL(imageURL)
+            //   iconView.sd_setImageWithURL(imageURL)
+            
+            /*
+            1.图片显示不完整
+            2.图片没有居中显示
+            3.长图显示也会有一些问题
+            */
+            //self.iconView.frame = CGRect(origin: CGPointZero, size: image.size)
+            
+            /*
+            let size = self.displaySize(image)
+            self.iconView.frame = CGRect(origin: CGPointZero, size: size)
+            // 处理剧终显示
+            let y = (UIScreen.mainScreen().bounds.height - size.height) * 0.5
+            self.scrollview.contentInset = UIEdgeInsets(top: y, left: 0, bottom: y, right: 0)
+            
+            //self.iconView.frame = CGRect(origin: CGPoint(x: 0, y: y), size: size)
+            */
+            
+            // 1.重置属性
+            reset()
+            
+            // 2.显示菊花
+            activity.startAnimating()
+            
+            // 3.设置图片
             iconView.sd_setImageWithURL(imageURL) { (image, _, _, _) -> Void in
-                /*
-                1.图片显示不完整
-                2.图片没有居中显示
-                3.长图显示也会有一些问题
-                */
-                //self.iconView.frame = CGRect(origin: CGPointZero, size: image.size)
-        
-                /*
-                let size = self.displaySize(image)
-                self.iconView.frame = CGRect(origin: CGPointZero, size: size)
-                // 处理剧终显示
-                let y = (UIScreen.mainScreen().bounds.height - size.height) * 0.5
-                self.scrollview.contentInset = UIEdgeInsets(top: y, left: 0, bottom: y, right: 0)
+                // 4.隐藏菊花
+                self.activity.stopAnimating()
                 
-                //self.iconView.frame = CGRect(origin: CGPoint(x: 0, y: y), size: size)
-                */
-                
+                // 5.调整图片的尺寸和位置
                 self.setImageViewPostion()
                 
             }
+            
         }
     }
     //MARK: - 按照图片的宽高比计算图片显示的大小
@@ -90,15 +103,36 @@ class PhotoBrowserCell: UICollectionViewCell {
         // 1.添加子控件
         contentView.addSubview(scrollview)
         scrollview.addSubview(iconView)
+        contentView.addSubview(activity)
         
         // 2.布局子控件
         scrollview.frame = UIScreen.mainScreen().bounds
+        activity.center = contentView.center
+        
+        // 3.处理缩放
+        scrollview.delegate = self
+        scrollview.maximumZoomScale = 2.0
+        scrollview.minimumZoomScale = 0.5
         
     }
     
+    //MARK:   重置scrollview和imageview的属性
+    ///   重置scrollview和imageview的属性
+    private func reset()
+    {
+        // 重置scrollview
+        scrollview.contentInset = UIEdgeInsetsZero
+        scrollview.contentOffset = CGPointZero
+        scrollview.contentSize = CGSizeZero
+        
+        // 重置imageview
+        iconView.transform = CGAffineTransformIdentity
+    }
     // MARK: - 懒加载
     private lazy var scrollview: UIScrollView = UIScrollView()
     private lazy var iconView: UIImageView = UIImageView()
+    // 菊花
+    private lazy var activity: UIActivityIndicatorView =  UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.WhiteLarge)
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -106,3 +140,35 @@ class PhotoBrowserCell: UICollectionViewCell {
     
     
 }
+
+extension PhotoBrowserCell: UIScrollViewDelegate {
+    
+    //MARK:  告诉系统需要缩放哪个控件
+    ///  告诉系统需要缩放哪个控件
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView?
+    {
+        return iconView
+    }
+    
+    
+    //MARK:  重新调整配图的位置
+    ///  重新调整配图的位置
+    // view: 被缩放的视图
+    func scrollViewDidEndZooming(scrollView: UIScrollView, withView view: UIView?, atScale scale: CGFloat) {
+        print("scrollViewDidEndZooming")
+        
+        // 注意: 缩放的本质是修改transfrom, 而修改transfrom不会影响到bounds, 只有frame会受到影响
+        //        print(view?.bounds)
+        //        print(view?.frame)
+        
+        var offsetX = (UIScreen.mainScreen().bounds.width - view!.frame.width) * 0.5
+        var offsetY = (UIScreen.mainScreen().bounds.height - view!.frame.height) * 0.5
+        //        print("offsetX = \(offsetX), offsetY = \(offsetY)")
+        offsetX = offsetX < 0 ? 0 : offsetX
+        offsetY = offsetY < 0 ? 0 : offsetY
+        
+        scrollView.contentInset = UIEdgeInsets(top: offsetY, left: offsetX, bottom: offsetY, right: offsetX)
+    }
+    
+}
+
