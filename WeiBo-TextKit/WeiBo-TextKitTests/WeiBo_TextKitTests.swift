fdcbd94a41b4bd0b6c5a037dd9e1611d93fad5ef
4jchc <jiangjin5y24@163.com>
2016-02-13 22:50:30 +0800

    【WeiBo-TextKit】01 初始化

---
+++ WeiBo-TextKit/WeiBo-TextKitTests/WeiBo_TextKitTests.swift
@@ -0,0 +1,36 @@
+//
+//  WeiBo_TextKitTests.swift
+//  WeiBo-TextKitTests
+//
+//  Created by 蒋进 on 16/2/13.
+//  Copyright © 2016年 蒋进. All rights reserved.
+//
+
+import XCTest
+@testable import WeiBo_TextKit
+
+class WeiBo_TextKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
