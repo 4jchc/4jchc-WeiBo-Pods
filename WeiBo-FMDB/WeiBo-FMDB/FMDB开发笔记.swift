2e37006937e1f8e7b2d33c49a1dc5f166ab80395
4jchc <jiangjin5y24@163.com>
2016-02-14 21:30:31 +0800

    【WeiBo-FMDB】02 添加FMDB-动态库sql-桥接

---
+++ WeiBo-FMDB/WeiBo-FMDB/FMDB开发笔记.swift
@@ -0,0 +1,14 @@
+//
+//  FMDB开发笔记.swift
+//  WeiBo-FMDB
+//
+//  Created by 蒋进 on 16/2/14.
+//  Copyright © 2016年 蒋进. All rights reserved.
+//
+
+import UIKit
+
+
+    /*
+    添加FMDB-动态库sql-桥接
+    */
---
+++ WeiBo-FMDB/WeiBo-FMDB/Swift-Bridge.h
@@ -0,0 +1,9 @@
+//
+//  ViewController.swift
+//  WeiBo-FMDB
+//
+//  Created by 蒋进 on 16/2/14.
+//  Copyright © 2016年 蒋进. All rights reserved.
+//
+
+#import "FMDB.h"
